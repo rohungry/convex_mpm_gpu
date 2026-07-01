531fac09be927be77d53a352bd077aa0a1be2474
Rohun Kshirsagar <rohunsheer@gmail.com>
2026-06-30 22:51:12 -0700

    move HvP declarations inside GpuMpmSolver class

--- multibody/gpu_mpm/cuda_mpm_solver.cuh
+++ multibody/gpu_mpm/cuda_mpm_solver.cuh
@@ -13,10 +13,7 @@ namespace drake {
 namespace multibody {
 namespace gmpm {
 
-void PrecomputeContactHessian(GpuMpmState<T>* s, const T& dt) const;
-void ApplyHessian(GpuMpmState<T>* s, const T* p, T* Hp) const;
-void AssembleContactGradient(GpuMpmState<T>* s, const T& dt, T* g_out) const;
-bool RunHvpSelfTests(GpuMpmState<T>* s, const T& dt) const;
+
 
 // NOTE(changyu): this solver should be stateless, all the required data should be initialized and stored in `GpuMpmState`.
 // NOTE(changyu): `GpuMpmSolver` is responsive to launch cuda kernels in `cuda_mpm_kernels.cuh`.
@@ -34,6 +31,11 @@ public:
     void Dump(const GpuMpmState<T> &state, std::string filename) const;
     void CopyContactPairs(GpuMpmState<T> *state, const MpmParticleContactPairs<T> &contact_pairs) const;
     void UpdateContact(GpuMpmState<T> *state, const T& dt) const;
+
+    void PrecomputeContactHessian(GpuMpmState<T>* s, const T& dt) const;
+    void ApplyHessian(GpuMpmState<T>* s, const T* p, T* Hp) const;
+    void AssembleContactGradient(GpuMpmState<T>* s, const T& dt, T* g_out) const;
+    bool RunHvpSelfTests(GpuMpmState<T>* s, const T& dt) const;
 };
 
 }  // namespace gmpm
